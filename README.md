2123ba9116d2a1e168721affa4fdeb59b40a4bf5
Igor Juan <igor51juan@gmail.com>
2024-01-11 17:15:32 -0500

    19:14 11/01
    Adição  do projeto "Advinhacao"
    Criação do README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Projetos-em-C
- 
+ #### Pequenos projetos e testes simples em linguagem C.
+
+ ### Descrição:
+
+###### Os programas apresentados neste repositório são projetos e testes escritos na linguagem C.
+###### Os programas contidos neste repositório foram utilizados para aprendizado e o entendimento de como a linguagem C funciona e suas sintaxes.
+###### Os programas contidos neste repositoório foram desenvolvidos na plataforma LINUX e UNIX em geral, podendo ocasionar erro em tentar utilizar a plataforma Windows!
---
+++ src/advinhacao.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() {
+    int numerosecreto = 42;
+    int chute;
+    int tentativas = 1;
+    double pontos = 100;
+
+    printf("******************************************\n");
+    printf("*Bem vindo ao nosso jogo de adivinhação*\n");
+    printf("******************************************\n");
+
+    while (1) {
+        printf("tentativa %d\n", tentativas);
+        printf("Qual é o seu chute?\n ");
+        scanf("%d", &chute);
+
+        if (chute < 0) {
+            printf("Você não pode chutar números negativos\n");
+            continue;
+        }
+
+        if (chute == numerosecreto) {
+            printf("Parabéns! Você acertou\n");
+            printf("Jogue novamente!\n");
+            break;
+        } else if (chute > numerosecreto) {
+            printf("O seu chute foi maior que o número secreto\n");
+        } else {
+            printf("Seu chute foi menor que o número secreto\n");
+        }
+        
+        tentativas++;
+        double pontosPerdidos = (chute - numerosecreto) / 2.0;
+        pontos = pontos - pontosPerdidos;
+    }
+
+    printf("Fim de jogo!\n");
+    printf("Você acertou em %d tentativas\n", tentativas);
+    printf("A sua pontuação foi de %.1f", pontos);
+
+    return 0;
+}
